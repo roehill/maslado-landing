56e4436f32ab7273fd0a85638ca8ec45044fbce0
roehill <roehilldev@gmail.com>
2024-04-19 21:57:07 +0200

    Regulamin - zasady ogólne

---
+++ src/app/terms-and-conditions/page.jsx
@@ -0,0 +1,50 @@
+import styles from "./termAndConditions.module.css";
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Regulamin korzystania z Platformy Maslado obowiązujący od dnia 1 maja 2024 roku</h1>
+      <h2>§ 1. Zasady ogólne</h2>
+      <p>
+        Niniejszy regulamin (zwany dalej "Regulaminem") definiuje warunki i zasady korzystania z Platformy Maslado
+        znajdującej się pod adresem internetowym https://www.app.maslado.com (zwanej dalej "Platformą") oraz usług
+        świadczonych drogą elektroniczną przez Administratora.
+      </p>
+      <p>
+        Właścicielem Platformy jest Martyna Tomaszewska prowadząca działalność gospodarczą pod nazwą "MT FOTO Martyna
+        Tomaszewska", znajdującą się w Centralnej Ewidencji i Informacji o Działalności Gospodarczej (zwany dalej
+        "Administratorem"). <br></br>NIP: 5662017864<br></br> REGON: 366736364
+      </p>
+      <p>
+        Umożliwienie korzystania z Platformy jest usługą świadczoną drogą elektroniczną przez Administratora na rzecz
+        użytkowników, w rozumieniu:<br></br> - ustawy z dnia 18 lipca 2002 r. o świadczeniu usług drogą elektroniczną
+        (Dz.U.2020.344 t.j. z dnia 2020.03.03), które odbywa się na podstawie Regulaminu,<br></br>- art. 71 ustawy z
+        dnia 23 kwietnia 1964 r. Kodeks cywilny (zwany dalej: „Kodeksem cywilnym”),<br></br>- ustawy z dnia 16 lipca
+        2004 r. Prawo telekomunikacyjne (Dz.U. 2017 poz. 1907 t.j.), (zwanej dalej „Prawem telekomunikacyjnym”),
+        <br></br>- rozporządzenia Parlamentu Europejskiego i Rady UE 2016/679 z 27 kwietnia 2016 roku w sprawie ochrony
+        osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz
+        uchylenia dyrektywy 95/46/WE, (zwanej dalej „RODO”).
+      </p>
+      <p>
+        Przed rozpoczęciem korzystania z Platformy, Usługobiorca zobowiązany jest zapoznać się z Regulaminem oraz
+        Polityką prywatności.
+      </p>
+      <p>
+        Założenie konta na Platformie przez Usługobiorcę jest równoważne z zaakceptowaniem wszystkich postanowień
+        niniejszego Regulaminu.{" "}
+      </p>
+
+      <h2>§ 2. Definicje</h2>
+      <p>Administrator - właściciel serwisu</p>
+      <p>Platforma - </p>
+      <p>Regulamin - </p>
+      <p>Polityka prywatności - Fotograf lub Klient</p>
+      <p>Kodeks cywilny - </p>
+      <p>Prawo telekomunikacyjne - </p>
+      <p>RODO - </p>
+      <p>Usługobiorca - Fotograf lub Klient</p>
+    </div>
+  );
+};
+
+export default page;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,25 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.footerBody}>
-        Masz pytania do naszego zespołu?<br></br> Skorzystaj z{" "}
-        <Link href="https://app.maslado.com/contact">formularza kontaktowego</Link>, lub napisz email bezpośrednio na{" "}
-        <span className={styles.brand}>kontakt@maslado.com</span>
-      </p>
+      <div className={styles.footerBody}>
+        <p className={styles.contact}>
+          Masz pytania do naszego zespołu?<br></br> Skorzystaj z{" "}
+          <Link className={styles.s} href="https://app.maslado.com/contact">
+            formularza kontaktowego
+          </Link>
+          , lub napisz email bezpośrednio na <span className={styles.brand}>kontakt@maslado.com</span>
+        </p>
+        <div className={styles.links}>
+          {" "}
+          <p>2024 © Maslado. </p>
+          <Link href="https://maslado.com/terms-and-conditions" className={styles.link}>
+            Regulamin
+          </Link>
+          <Link href="https://maslado.com/privacy-policy" className={styles.link}>
+            Polityka prywatności
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
