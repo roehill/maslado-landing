4508854eebfe0d3405c63eaf91419b816b36ec47
roehill <roehilldev@gmail.com>
2024-04-29 12:44:10 +0200

    Regulamin i polityka prywatności

---
+++ src/app/privacy-policy/page.jsx
@@ -0,0 +1,83 @@
+import styles from "./privacyPolicy.module.css";
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Polityka Prywatności</h1>
+      <p>
+        Administratorem danych osobowych jest Martyna Tomaszewska prowadząca działalność gospodarczą pod nazwą "MT FOTO
+        Martyna Tomaszewska", znajdującą się w Centralnej Ewidencji i Informacji o Działalności Gospodarczej. <br></br>
+        NIP: 5662017864<br></br> REGON: 366736364
+      </p>
+      <p>
+        Niniejsze Zasady ochrony prywatności wyjaśniają, jak Platforma Maslado i jej jednostki stowarzyszone gromadzą,
+        przetwarzają i wykorzystują informacje o użytkowniku (które nazywamy „danymi osobowymi”) oraz jak nimi
+        administrują.
+      </p>
+      <p>
+        Podczas korzystania z Platformy Maslado gromadzimy następujące dane osobowe: <br></br>a) informacje
+        uwierzytelniające i identyfikacyjne (np. imię i nazwisko, adres e-mail i hasło). Informacje niezbędne do
+        skonfigurowania konta użytkownika i zalogowania się, ale istnieje również możliwość dokonywania zakupów bez
+        konieczności tworzenia hasła;<br></br> b) podstawowe dane osobowe (np. imię i nazwisko,);<br></br> c) dane
+        kontaktowe (np. adres, numer telefonu i adres e-mail, adres korespondencyjny) aby dostarczać produkty;<br></br>
+        d) dane o płatnościach (np. dane karty kredytowej lub tokeny płatności od zewnętrznych dostawców usług
+        płatniczych, takich jak PayPal). Używamy ich do przetwarzania zamówienia.
+      </p>
+      <p>
+        Podczas korzystania z Platformy Maslado przez urządzenie gromadzimy także automatycznie pewne dane osobowe. Te
+        dane osobowe obejmują: <br></br>a) dane analityczne (np. historia używania aplikacji), <br></br>b) elementy
+        wprowadzone przez profil (np. szczegóły zakupu, informacje o galeriach i klientach). <br></br>c) dane urządzenia
+        (np. adres MAC, adres IP, dane interfejsu Bluetooth i identyfikatory reklamowe).
+      </p>
+      <p>
+        Dane osobowe użytkownika ujawniamy w następujący sposób: <br></br>a) za jego zgodą; <br></br>b) jednostkom
+        stowarzyszonym; <br></br>c) niepowiązanym z nami zewnętrznym Sprzedawcom i Partnerom biznesowym; <br></br>d)
+        osobom trzecim, które dostarczają narzędzi i świadczą usługi pomagające nam lepiej zrozumieć preferencje
+        użytkownika dotyczące ofert; <br></br>e) aby zgłaszać lub windykować zadłużenie należne Sprzedającym lub innym
+        Partnerom biznesowym; <br></br>f) w zakresie koniecznym do wywiązania się ze zobowiązań umownych wobec
+        użytkownika i Sprzedawców lub Partnerów biznesowych; <br></br>g) w celu zwalczania oszustw lub działalności
+        przestępczej oraz w celu ochrony praw naszych lub Jednostek stowarzyszonych
+        <br></br>h) w odpowiedzi na wezwanie sądowe lub podobną procedurę prawną, w tym dla organów ścigania, organów
+        regulacyjnych i sądów w zakresie niezbędnym do spełnienia tych zobowiązań prawnych;
+      </p>
+      <p>
+        Wdrożyliśmy program ochrony informacji obejmujący administracyjne, techniczne i fizyczne mechanizmy kontroli,
+        obejmujące standardowo wykorzystywaną w branży technologię szyfrowania danych, które mają na celu zapewnienie
+        ochrony danych osobowych użytkownika.{" "}
+      </p>
+      <p>
+        Dane osobowe będziemy przechowywać tak długo, jak będzie aktywne konto użytkownika lub będą one potrzebne do
+        świadczenia usług.
+      </p>
+      <p>
+        Pliki cookie. Można zarządzać sposobem obsługi plików cookie przez przeglądarkę. Można także zarządzać sposobem,
+        w jaki urządzenie mobilne i przeglądarka mobilna udostępniają informacje o urządzeniu oraz informacje
+        zgromadzone na urządzeniu, a także tym, jak przeglądarka mobilna obsługuje pliki cookie.{" "}
+      </p>
+      <p>
+        Użytkownik ma prawo uzyskać dostęp, aktualizować, poprawiać i usuwać swoje dane osobowe, które stanowią część
+        jego profilu. Może to zrobić logując się na konto i przechodząc do zakładki "ustawienia". Aktualizowanie danych
+        osobowych pomaga zapewnić, że możemy uszanować preferencje użytkownika oraz oferować towary i usługi, które są
+        najbardziej odpowiednie dla konkretnego użytkownika.
+      </p>
+      <p>
+        Zgodnie z obowiązującym prawem użytkownik może (i) żądać dostępu do wszelkich danych osobowych, które posiadamy
+        o nim; (ii) zażądać ich aktualizacji, sprostowania, usunięcia lub zablokowania; (iii) zażądać usunięcia przez
+        nas danych osobowych, które posiadamy o nim; (iv) zażądać ograniczenia przetwarzania przez nas danych osobowych;
+        oraz (v) zażądać przekazania użytkownikowi albo osobie trzeciej kopii określonych danych osobowych użytkownika
+        (co określa się jako prawo do „przenoszenia danych”). Można również wyrazić sprzeciw wobec któregokolwiek ze
+        stosowanych przez nas sposobów wykorzystywania danych osobowych opisanych w niniejszych Zasadach, w tym wobec
+        działań marketingowych. Użytkownik może wycofać swoją zgodę na przetwarzanie danych osobowych w zakresie, w
+        jakim zgoda była wymagana i w jakim została udzielona.
+      </p>
+      <p>
+        Przetwarzamy przekazane nam dane osobowe gromadzone oraz administrujemy nimi, aby: <br></br>a) Utworzyć konto
+        podczas rejestracji i umożliwić logowanie, co jest niezbędne, abyśmy mogli świadczyć usługi. <br></br>b)
+        Obsługiwać, utrzymywać i ulepszać Platformę poprzez analizowanie; <br></br>c) Weryfikować wszelkie naruszenia
+        regulaminu i przeciwdziałać im.
+      </p>
+    </div>
+  );
+};
+
+export default page;
--- src/app/terms-and-conditions/page.jsx
+++ src/app/terms-and-conditions/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Regulamin korzystania z Platformy Maslado obowiązujący od dnia 1 maja 2024 roku</h1>
-      <h2>§ 1. Zasady ogólne</h2>
+      <h2>§ 1. Postanowienia wstępne</h2>
       <p>
         Niniejszy regulamin (zwany dalej "Regulaminem") definiuje warunki i zasady korzystania z Platformy Maslado
         znajdującej się pod adresem internetowym https://www.app.maslado.com (zwanej dalej "Platformą") oraz usług
@@ -36,13 +36,149 @@ const page = () => {
 
       <h2>§ 2. Definicje</h2>
       <p>Administrator - właściciel serwisu</p>
-      <p>Platforma - </p>
-      <p>Regulamin - </p>
-      <p>Polityka prywatności - Fotograf lub Klient</p>
-      <p>Kodeks cywilny - </p>
-      <p>Prawo telekomunikacyjne - </p>
-      <p>RODO - </p>
-      <p>Usługobiorca - Fotograf lub Klient</p>
+      <p>Konto - panel utworzony na Platformie Maslado umożliwiający Usługobiorcy korzystanie z usług.</p>
+      <p>
+        Platforma - aplikacja znajdująca się pod adresem www.app.maslado.com za pomocą, której Administrator świadczy
+        usługi Usługobiorcom
+      </p>
+      <p>Regulamin - regulamin korzystania z Platformy Maslado.</p>
+      <p>
+        Polityka prywatności - dokument zawierający informacje o przetwarzaniu danych osobowych Usługobiorców przez
+        Administratora
+      </p>
+      <p>
+        Kodeks cywilny - akt normatywny, w którym znajdziemy regulacje dotyczące stosunków cywilnoprawnych między
+        równouprawnionymi podmiotami.
+      </p>
+      <p>
+        Prawo telekomunikacyjne - akt prawny, regulujący zasady funkcjonowania rynku telekomunikacyjnego w warunkach
+        gospodarki wolnorynkowej z uwzględnieniem prawodawstwa Unii Europejskiej.
+      </p>
+      <p>Usługobiorca - osoba, która zarejestrowała się w Platformie Maslado.</p>
+
+      <h2>§ 3. Wymagania techniczne</h2>
+      <p>
+        W celu prawidłowego korzystania z Platformy konieczne jest posiadanie przez Usługobiorcę urządzeń
+        umożliwiających korzystanie z seci Internet oraz dostępu do odpowiednio wydajnego łącza internetowego.
+      </p>
+      <p>
+        W celu rejestracji Konta oraz późniejszego otrzymywania wiadomości automatycznych z Platformy, Usługobiorca musi
+        posiadać aktywne konto poczty elektronicznej.
+      </p>
+      <p>
+        Usługodawca informuje, że pomimo zastosowania odpowiednich zabezpieczeń, korzystanie z sieci Internet oraz usług
+        świadczonych drogą elektroniczną może być zagrożone dostaniem się do systemu teleinformatycznego oraz urządzenia
+        Usługobiorcy, szkodliwego oprogramowania lub uzyskaniem dostępu do danych znajdującym się na tym urządzeniu
+        przez osoby trzecie. W celu zminimalizowania zagrożenia Administrator zaleca stosowanie programów antywirusowych
+        lub środków chroniących identyfikację w sieci Internet.
+      </p>
+      <p>
+        Koszty wynikające z posługiwania się przez Fotografa środkami porozumiewania się na odległość (koszty połączenia
+        z siecią Internet, SMS, połączeń telefonicznych) celem skorzystania z Usług Administratora, ponosi Usługobiorca
+        zgodnie z cennikami dostawców, z których usług korzysta.
+      </p>
+      <p>
+        Administrator zapewnia, że dołoży wszelkich starań, aby Platforma działała poprawnie i w pełnej funkcjonalności
+        w każdym czasie, jednakże zastrzega sobie prawo do czasowego wyłączenia Platformy lub jej niektórych funkcji w
+        celu ich aktualizacji. Administrator udostępnia Platformę Usługobiorcom w wymiarze 24/7, z prawem do krótkich
+        przerw serwisowych.
+      </p>
+
+      <h2>§ 4. Zasady ogólne</h2>
+      <p>Utworzenie i korzystanie z Konta jest dobrowolne i bezpłatne.</p>
+      <p>
+        Usługobiorca nie ma prawa udostępniania swojego Konta innej osobie. Usługobiorca zostaje niniejszym
+        poinformowany, że Platforma może zawierać Materiały, których wykorzystanie może wymagać (oprócz uzyskania
+        licencji na korzystanie z autorskich praw majątkowych) uzyskania pozwoleń od osób lub podmiotów trzecich w
+        odniesieniu do prezentowanych treści. Użytkownik jest wyłącznie odpowiedzialny za ustalenie czy dla danego,
+        planowanego przez niego korzystania potrzebne są jakiekolwiek dodatkowe pozwolenia. W przypadku wątpliwości
+        obowiązkiem Usługobiorcy jest zasięgnięcie porady właściwej instytucji zarządzającej prawami lub/oraz
+        kompetentnego podmiotu świadczącego profesjonalnie usługi doradztwa prawnego.
+      </p>
+      <p>Usługobiorca ponosi pełną odpowiedzialność za Materiały zamieszczane przez niego na Platformie.</p>
+      <p>
+        Usługobiorca zobowiązany jest do korzystania z usług świadczonych przez Administratora w sposób zgodny z
+        przepisami prawa powszechnie obowiązującego, postanowieniami niniejszego Regulaminu, a także z dobrymi
+        obyczajami.
+      </p>
+      <p>
+        Wszelkie działania mające na celu utrudnienie lub destabilizowanie działania Platformy są zabronione i będą
+        skutkować blokadą Konta.
+      </p>
+      <p>
+        W celu skorzystania z usług świadczonych przez Administratora na Platformie wymagana jest rejestracja Konta.
+      </p>
+      <p>
+        Administrator udostępnia Platformę Usługobiorcom w zakresie i na waurnkach przewidzianych przez niniejszy
+        Regulamin oraz Cennik, które są udostępnione na strone www.maslado.com
+      </p>
+      <p>Usługobiorca może posiadać jednocześnie tylko jedno aktywne Konto na Platformie.</p>
+      <p>
+        Usługobiorca może zamieszczać na Platformie tylko fotografie do których posiada prawa autorskie. Korzystanie
+        przez Usługobiorców z materiałów chronionych prawami autorskimi, w tym ich kopiowanie, przesyłanie oraz
+        publiczne udostępnianie w Internecie odbywa się za zgodą uprawnionych podmiotów. Usługobiorcy ponoszą całkowitą
+        odpowiedzialność za wszelkie szkody powstałe w wyniku ich sprzecznego z powyższym zastrzeżeniem działania bądź
+        zaniechania.
+      </p>
+      <p>Usługobiorca zobowiązuje się nie udostępniać osobom trzecim hasła do swojego Konta.</p>
+
+      <h2>§ 5. Rejestracja</h2>
+      <p>Utworzenie Konta odbywa się automatycznie na podstawie danych przekazanych na Platformę.</p>
+      <p>
+        2. Utworzenie Konta wymaga, by Usługobiorca podał następujące dane służące do identyfikacji: <br></br>• nazwa
+        organizacji/firmy, <br></br>• imię, <br></br>• adres email, <br></br>• hasło, <br></br>• zapoznał się z
+        Regulaminem i potwierdził akceptację postanowień Regulaminu;
+        <br></br>• aktywował Konto poprzez kliknięcie linku aktywacyjnego otrzymanego w wiadomości email;
+      </p>
+      <p>Usługobiorca zobowiązany jest do podania prawdziwych danych osobowych, w tym danych kontaktowych.</p>
+      <p>
+        Nie jest dozwolone: <br></br>a) naruszanie technicznych zabezpieczeń Platformy oraz celowe omijanie środków
+        zastosowanych w celu zapobieżenia bądź ograniczenia dostępu do Platformy lub Materiałów, <br></br>b) stosowanie
+        jakichkolwiek urządzeń, narzędzi programistycznych, algorytmów lub procedur, które mogłyby zakłócać prawidłowe
+        funkcjonowanie Platformy.
+      </p>
+
+      <h2>§ 6. Odpowiedzialność Podmiotu prowadzącego Platformę </h2>
+      <p>
+        Podmiot prowadzący Platformę nie ponosi odpowiedzialności za jakiekolwiek działania Usługobiorcy, ani szkody z
+        nich wynikające.
+      </p>
+      <p>
+        Podmiot prowadzący Platformę nie ponosi odpowiedzialności za treść, formę oraz błędne lub nieaktualne informacje
+        zawarte w Materiałach dostępnych w modułach.
+      </p>
+      <p>
+        Podmiot prowadzący Platformę nie ponosi odpowiedzialności za wykorzystanie Materiałów przez Usługobiorcę, sposób
+        korzystania z Konta, ani za jakiekolwiek ewentualne szkody związane z korzystaniem przez Usługobiorcę z
+        Platformy, w tym z Materiałów, ani za skutki wynikłe z ujawnienia Hasła osobom trzecim, niezależnie od przyczyny
+        tego ujawnienia.
+      </p>
+      <p>
+        Podmiot prowadzący Platformę stosuje środki techniczne i organizacyjne, zapewniające ochronę przetwarzanych
+        danych osobowych, odpowiednią do zagrożeń oraz kategorii danych objętych ochroną.
+      </p>
+      <p>
+        Podmiot prowadzący Platformę, Administrator zobowiązują się zabezpieczyć dane osobowe przed ich przypadkowym lub
+        niezgodnym z prawem zniszczeniem, utratą, modyfikacją, udostępnieniem osobom nieupoważnionym, zabraniem przez
+        osobę nieuprawnioną, przetwarzaniem z naruszeniem przepisów rozporządzenia Parlamentu Europejskiego i Rady (UE)
+        2016/679 w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego
+        przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO).
+      </p>
+
+      <h2>§ 7. Postanowienia końcowe</h2>
+      <p>
+        Usługobiorca oświadcza, iż zapoznał się z Regulaminem oraz Polityką Prywatności, rozumie treść tych dokumentów i
+        w pełni je akceptuje oraz zobowiązuje się do ich przestrzegania.
+      </p>
+      <p>
+        Administratorowi przysługuje prawo zmiany Regulaminu poprzez publikację jego zaktualizowanej treści na 14 dni
+        przed wejściem w życie zmiany Regulaminu oraz powiadomienie Usługobiorców za pomocą wiadomości email.
+      </p>
+      <p>
+        W terminie 7 dni od ogłoszenia informacji o zmianie Regulaminu, Usługobiorcy przysługuje prawo jednostronnego
+        rozwiązania Umowy.{" "}
+      </p>
+      <p>W sprawach nieuregulowanych Regulaminem zastosowanie znajdują powszechnie obowiązujące przepisy prawa.</p>
     </div>
   );
 };
